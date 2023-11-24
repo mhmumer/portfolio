8a389285c0c4ea08782884f426cc1fc333250571
Muhammad Umer <74130137+mhmumer@users.noreply.github.com>
2023-11-24 21:00:33 +0500

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,6 +28,8 @@ let header = $(`
    <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Projects</a></li>
    
    <li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Education</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="https://mhmumer.github.io/Portfolio-website/images/cv.png">View CV</a></li>
+   
    
    <li class="nav-item">
    <input type="checkbox" id="dark_toggler" class="dark_toggler" aria-label="Toggle Light Mode" onclick="toggle_light_mode()" checked>
